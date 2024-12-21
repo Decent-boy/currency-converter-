0aba824677b29eeb14343847765911625a5ebc52
Decent-boy <aatifiqbal836@gmail.com>
2024-12-22 00:06:53 +0500

    Add files via upload

---
+++ practice test.py
@@ -0,0 +1,16 @@
+def currentcy(usd):
+    rs=usd*280
+    print(f"PKR : {rs}")
+
+while True:
+    try:
+        user=input("are you wnated to see currncey of pkr\ntype y: ")
+        if user.lower().strip()=="y":
+            usd=int(input("Enter usd dollors: "))
+            currentcy(usd)
+        else:
+            quit()
+
+            # print("sorry")
+    except ValueError:
+        print("invalid")
